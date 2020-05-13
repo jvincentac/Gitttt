bcbcfe7b1e7b0e6da908cfc660192f8e7c943a0e
Vincent Alexander Christian <jvincentalexnder@gmail.com>
2020-05-13 15:28:33 +0700

    Initial Commit

---
+++ Gitttt/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Gitttt
+//
+//  Created by Vincent Alexander Christian on 13/05/20.
+//  Copyright © 2020 Vincent Alexander Christian. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
